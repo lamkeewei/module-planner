a71ff0482df807f7f36a341be95b74519d4adf97
Lam Kee Wei <keewei.lam@gmail.com>
2014-05-21 02:35:43 +0800

    Updated with toggle for selecting status

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,6 +1,11 @@
 'use strict';
 
 angular.module('modulePlannerApp')
-  .controller('MainCtrl', function ($scope, User) {
+  .controller('MainCtrl', function ($scope, User, _) {
     $scope.planner = User.planner();
+
+    $scope.isExemption = function(course){
+      var match = _.indexOf(course.category, 'Exemption');
+      return match >= 0;
+    };
   });
