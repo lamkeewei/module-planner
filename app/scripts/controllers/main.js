3f943b1ef734668681e2c15fd3d1a60d7d06edce
Lam Kee Wei <keewei.lam@gmail.com>
2014-05-21 16:04:48 +0800

    Added modal scaffolding

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,11 +1,17 @@
 'use strict';
 
 angular.module('modulePlannerApp')
-  .controller('MainCtrl', function ($scope, User, _) {
+  .controller('MainCtrl', function ($scope, User, _, $modal) {
     $scope.planner = User.planner();
 
     $scope.isExemption = function(course){
       var match = _.indexOf(course.category, 'Exemption');
       return match >= 0;
+    };
+
+    $scope.openModal = function(){
+      var modalInstace = $modal.open({
+        template: '<div class="course-modal"></div>'
+      });
     };
   });
