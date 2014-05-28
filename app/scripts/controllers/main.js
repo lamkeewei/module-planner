956d4925a6fcb96af45b2f6192255f590a48ea2a
Lam Kee Wei <keewei.lam@gmail.com>
2014-05-28 17:58:36 +0800

    Fixed backend double count

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -73,25 +73,6 @@ angular.module('modulePlannerApp')
 
           User.deselectCourse(params, function(){
             var prev = category.courses[index];
-            
-            if (prev.doubleCount.length > 0) {
-              angular.forEach($scope.planner, function(category){
-                var categoryType = category.type;
-
-                angular.forEach(prev.doubleCount, function(doubleCount){
-                  // Remove appropriate number of courses
-                  if (doubleCount.replace === categoryType) {
-                    category.courses.push({});
-                  }
-                  // Add appropriate number of courses
-                  if (doubleCount.freeUp === categoryType) {
-                    var courses = category.courses;
-                    courses.splice(courses.length - 1, 1);
-                  }
-                });
-              });
-            }
-
             category.courses[index] = {};
           });
 
@@ -103,25 +84,8 @@ angular.module('modulePlannerApp')
           previous: category.courses[index]._id
         };
 
-        User.selectCourse(data, function(){
+        User.selectCourse(data, function(data){
           category.courses[index] = selected;
-          if (selected.doubleCount.length > 0) {
-            angular.forEach($scope.planner, function(category){
-              var categoryType = category.type;
-
-              angular.forEach(selected.doubleCount, function(doubleCount){
-                // Remove appropriate number of courses
-                if (doubleCount.replace === categoryType) {
-                  var courses = category.courses;
-                  courses.splice(courses.length - 1, 1);
-                }
-                // Add appropriate number of courses
-                if (doubleCount.freeUp === categoryType) {
-                  category.courses.push({});
-                }
-              });
-            });
-          }
         });
       }, function(reason){
         // console.log('Dismissed:', reason);
